9078c147a61837d4439b62fb69fbfe22ad936461
Cleyton Mendes <cleyton.mendest@gmail.com>
2022-01-07 19:17:30 -0300

    ajusta array fornec

--- src/components/Fornecedores/Fornecedores.jsx
+++ src/components/Fornecedores/Fornecedores.jsx
@@ -2,17 +2,54 @@ import Image1 from './images/image1.jpg'
 import Image2 from './images/image2.jpg'
 import Image3 from './images/image3.png'
 
-const ImageBanner = [
+const Fornecedores = [
     {
+        id:1,
         src: Image1
     },
     {
         id:2,
-        "src":Image2
+        src:Image2
     },
     {
         id:3,
-        "src":Image3
+        src:Image3
+    },
+    {
+        id:1,
+        src: Image1
+    },
+    {
+        id:2,
+        src:Image2
+    },
+    {
+        id:3,
+        src:Image3
+    },
+    {
+        id:1,
+        src: Image1
+    },
+    {
+        id:2,
+        src:Image2
+    },
+    {
+        id:3,
+        src:Image3
+    },
+    {
+        id:1,
+        src: Image1
+    },
+    {
+        id:2,
+        src:Image2
+    },
+    {
+        id:3,
+        src:Image3
     },
 ]
-export default ImageBanner
+export default Fornecedores
